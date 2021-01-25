8676b24c78bb8de38c613022dcb4b8db5b7df1f5
Sarthak Joshi <iamsarthakjoshi@gmail.com>
2021-01-25 13:35:50 +1100

    Added TailwindCSS config files + Basic Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,12 @@
 import { ReactElement } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App(): ReactElement<HTMLElement> {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>In Progress! :D</p>
+        <span className="w-32 h-32 rounded-full mx-auto bg-gray-100">🙏</span>
+        <p className="">In Progress! :D</p>
       </header>
     </div>
   );
